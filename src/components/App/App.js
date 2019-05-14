93750602c083ada6aac836999b773d63343babfa
Isaac Morerod <isaacmorerod@isaacsMacBookPro.local>
2019-05-14 14:07:23 -0500

    airline name now displays on DOM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,15 +3,38 @@ import { connect } from 'react-redux';
 import mapReduxStateToProps from '../../modules/mapReduxStateToProps';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  
+
+    this.state = {
+      airlineName: ''
+    }
+  }
+
+  addAirline = (event) => {
+    this.props.dispatch({type: 'NAME', airlineName: this.state.airlineName});
+    console.log('BUTTON WORKING!')
+  }
+
+  changeAirlineName = (event) => {
+    this.setState({
+      airlineName: event.target.value
+    });
+  }
+  
   render() {
+    const array = this.props.reduxState.airlineReducer.map((airline, index) => {
+      return <li key={index}>{airline}</li>
+    });
+
     return (
       <div>
         <h1>Redux Airport</h1>
-        <input placeholder="Airelin Name" />
-        <button>Add Airline</button>
-        <table>
-          {/* Airlines should be listed here */}
-        </table>
+        <input placeholder="Airline Name" onChange={this.changeAirlineName} />
+        
+        <button onClick={this.addAirline}>Add Airline</button>
+        {array}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
-import {createStore} from 'redux';
+import {createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
 
+const airlineReducer = (state = [], action) => {
+    const airlineName = action.airlineName;
+    console.log(action);
+    if (action.type === 'NAME') {
+        return [
+            ...state,
+            airlineName
+        ];
+    }
+
+    return [
+        ...state
+    ];
+};
 
 
 
 
 
-const storeInstance = createStore();
+const storeInstance = createStore(
+    combineReducers({
+        airlineReducer
+    })
+);
 
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
